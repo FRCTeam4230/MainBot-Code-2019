bced910d560d59c3994c3bd3f0eb0df2b4f8196c
Max Hahn <hahn2219@marshallschool.org>
2019-02-13 20:26:14 -0600

    RobotMap.java: Add initial robot mappings

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,6 +3,20 @@ package frc.robot;
 
 
 public class RobotMap {
-
-  
+    public static class CAN {
+        public static int compressor = 0;
+        public static int driveLH1 = 1;
+        public static int driveLH2 = 2;
+        public static int driveRH1 = 3;
+        public static int driveRH2 = 4;
+        public static int driveClimber = 5;
+        public static int ballIntake = 6;
+        public static int linSlide = 7;
+    }
+    public static class PCM {
+        public static int hatchRelease = 6;
+        public static int hatchLower = 3;
+        public static int climberFront = 4;
+        public static int climberBack = 5;
+    }
 }
