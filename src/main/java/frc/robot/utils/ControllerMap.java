2075cea4420cc98391a3a966f74b4b4a1140aedb
Max Hahn <hahn2219@marshallschool.org>
2019-02-13 20:26:45 -0600

    ControllerMap.java: Add controller mappings

---
+++ src/main/java/frc/robot/utils/ControllerMap.java
@@ -0,0 +1,25 @@
+package frc.robot.utils;
+
+public class ControllerMap {
+    public  static class buttons {
+        public static int Y = 4;
+        public static int X = 3;
+        public static int A = 1;
+        public static int B = 2;
+        public static int lShoulder = 5;
+        public static int rShoulder = 6;
+        public static int RS = 6;
+        public static int left = 7;
+        public static int right = 8;
+        public static int lStick = 9;
+        public static int rStick = 10;
+    }
+    public static class analog {
+        public static int LX = 0;
+        public static int LY = 1;
+        public static int LT = 2;
+        public static int RT = 3;
+        public static int RX = 4;
+        public static int RY = 5;
+    }
+}
