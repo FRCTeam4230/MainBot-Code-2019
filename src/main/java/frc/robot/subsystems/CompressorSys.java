5902b42e3fb035841061a432f42e4764e8f86afe
Max Hahn <hahn2219@marshallschool.org>
2019-02-15 18:41:43 -0600

    Compressor: Add pressure sensor
    Set output to Dashboard for now.
    
    Closes #31

--- src/main/java/frc/robot/subsystems/CompressorSys.java
+++ src/main/java/frc/robot/subsystems/CompressorSys.java
@@ -1,14 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 public class CompressorSys extends Subsystem {
 
     private Compressor compressor;
+    private AnalogInput pressureSensor;
 
     public CompressorSys() {
         compressor = new Compressor(0);
+        pressureSensor = new AnalogInput(RobotMap.AnalogIn.pressureSensor);
         this.enable();
     }
 
@@ -26,8 +32,14 @@ public class CompressorSys extends Subsystem {
         compressor.stop();
     }
 
+    public double getPressure() {
+        double volts = pressureSensor.getVoltage();
+        double pressure = volts*RobotMap.Constants.pressureMult - RobotMap.Constants.pressureSub;
+        return (int) pressure;
+    }
+
     @Override
     public void periodic() {
-        // not implemented
+        SmartDashboard.putNumber("Pressure", getPressure());
     }
 }
